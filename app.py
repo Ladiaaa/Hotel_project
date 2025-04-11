1ce5e10278294696f2947786d979a44e73e6ca27
Mika <kanareika079@gmail.com>
2025-04-12 01:50:31 +0300

    Добавить функцию сохранения новых номеров в файл

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ import json
 def load_rooms():
     with open('rooms.json', 'r') as file:
         return json.load(file)
+
+# Сохраняем обновленные данные о номерах в файл
+def save_rooms(rooms):
+    with open('rooms.json', 'w') as file:
+        json.dump(rooms, file, indent=2)
